96ee8a455b0bd03dec98ff3d5027d46ebb6bda53
Your Name <1day@MacBook-Pro-khxng-Wasu.local>
2025-09-24 22:01:22 +0700

    Complete

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import express from "express";
 import morgan from 'morgan';
+import studentRoutes from "./routes/studentRoutes.js";
+import coureRoutes from "./routes/courseRoutes.js";
+import {type Request, type Response } from "express";
 
 const app: any = express();
 
@@ -7,9 +10,19 @@ const app: any = express();
 app.use(express.json());
 app.use(morgan('dev'));
 
+app.get("/", (req: Request, res: Response) => {
+  return res.status(200).json({
+    success: true,
+    message: "lab 15 API service successfully"
+  });
+});
+
+app.use("/me", studentRoutes);
+
+app.use("/api/v2", coureRoutes);
+
 app.listen(3000, () =>
   console.log("🚀 Server running on http://localhost:3000")
 );
-
 
 export default app;
